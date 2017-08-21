3fc9d855b92cab1f2b959e7a2e530bd2f3f2669b
eiji <eijimine@gmail.com>
2017-08-21 17:23:56 -0400

    Assignment Complete

--- ajax.js
+++ ajax.js
@@ -4,6 +4,7 @@ var runAjax = document.querySelector('#run_ajax');
 var runPingPong = document.querySelector('#run_ping_pong');
 var runCount = document.querySelector('#run_count');
 var runTime = document.querySelector('#run_time');
+var runCar = document.querySelector('#run_car');
 
 runAjax.addEventListener ( 'click', function() {
     $.ajax( {
@@ -62,7 +63,19 @@ runPingPong.addEventListener ( 'click', function() {
        newElement.innerHTML = data;
        section.append( newElement );
    });
+ });
 
+ runCar.addEventListener ( 'click', function() {
+     $.ajax( {
+       url: 'http://first-ajax-api.herokuapp.com/a_car',
+       method: 'GET',
+       dataType: 'HTML',
+     }).done(function(data) {
+       var newElement = document.createElement ('li');
+       var section = document.querySelector('#car')
+       newElement.innerHTML = data;
+       section.append( newElement );
+   });
  });
 
 
