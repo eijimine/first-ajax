7273ddf359247aea87a72915614255e897aa0829
eiji <eijimine@gmail.com>
2017-08-21 16:29:58 -0400

    Starting step 7

--- ajax.js
+++ ajax.js
@@ -1,5 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+var runAjax = document.querySelector('#run_ajax');
+var runPingPong = document.querySelector('#run_ping_pong');
+
+runAjax.addEventListener ( 'click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'text',
+    }).done(function (responseData) {
+  });
+});
+
+runPingPong.addEventListener ( 'click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text',
+    }).done(function(data) {
+        console.log('Request was succesful');
+        var newElement = document.createElement ('div');
+        var section = document.querySelector('#step3456')
+        newElement.innerHTML = data;
+        section.append( newElement );
+    }).fail(function() {
+        console.log('Request was unsuccesful');
+        var newElement = document.createElement ('div');
+        var section = document.querySelector('#step3456')
+        newElement.innerHTML = 'The data you are looking for is not available.';
+        section.append( newElement );
+    }).always(function functionName() {
+        console.log('Request Complete');
+    });
+
+ });
+
 
 });
